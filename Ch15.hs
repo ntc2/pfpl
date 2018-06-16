841e45821c451b61a8f1485b7e14ef4ce719d7d7
Nathan Collins <conathan@galois.com>
2018-06-16 10:37:08 -0700

    Explain the rules and their motivation in more depth
    And make it more obvious that I'm following the rules in the uses of
    the `E`-functors.

--- Ch15.hs
+++ Ch15.hs
@@ -13,17 +13,52 @@
 --
 -- Run 'main' to print examples.
 --
--- Note on exercises: the "rules" are that you can apply 'Fold',
--- 'Gen', 'rec', 'unfold', and 'fmap', and pattern match on functors
--- (like 'T_NatF'). You can't use pattern matching on 'Fold', or
--- 'Gen', or Haskell recursion. These rules keep our programs in
--- language described by Harper, altho embedded in Haskell.
+-- __Note on Exercises__: the point is to write programs in the language
+-- Harper describes in Ch 15, not to write Haskell programs. We're
+-- using Haskell to model the language in Ch 15, but Haskell has many
+-- features that the Ch 15 language does not have, such as pattern
+-- matching and general recursion, so we need to be careful that the
+-- solutions we write here correspond to programs in Harper's Ch 15
+-- language.
 --
--- Note on notation: I can't use dots ('.') in Haskell names, so I'm
+-- __Rules__ that make sure our solutions correspond to Ch 15
+-- programs:
+--
+-- - we can apply 'Fold', 'Gen', 'rec', 'unfold', and 'fmap'.
+--
+-- - we can't use pattern match on 'Fold', 'Gen', or use Haskell
+--   recursion.
+--
+-- - we can pattern match on the polynomial @t.tau@ functors we
+--   define, like 'T_NatF' __[1]__.
+--
+-- These rules keep our programs in the language described by Harper,
+-- altho embedded in Haskell. Of course, in defining our model of
+-- Harper's language, we are allowed to break the "rules" to
+-- /implement/ the primitives such as 'unfold' and 'rec'; the rules
+-- apply to /using/ the primitives to write programs in the modeled
+-- language.
+--
+-- __Footnote [1]__: why are we allowed to pattern match on the
+-- @t.tau@ functors we define when Harper's Ch 15 language doesn't
+-- have pattern matching? The reason this pattern matching is OK is
+-- that the polynomial @t.tau@ functors we define correspond to
+-- instances of 'E', and pattern matching on them corresponds to using
+-- the elimination functions 'l', 'r', and 'caseOf' (and 'v' and 'b',
+-- but these are purely modeling artifacts and corresponding to
+-- nothing in Harper's language). Indeed, if we use 'E' to model our
+-- @t.tau@ functors, then no pattern matching is necessary. For
+-- example, as an 'E' functor 'T_NatF' is 'Nat'F', and pattern
+-- matching on these 'T_NatF' is modeled by 'caseNat'F', which is a
+-- program in Harper's language, implemented in terms of 'l', 'r', and
+-- 'caseOf'.
+--
+-- __Note on Notation__: I can't use dots ('.') in Haskell names, so I'm
 -- using underscores instead. E.g. you should read @t_tau@ as
 -- @t.tau@. Also, in Haskell we do substitution by application, so
 -- e.g. @[tau'/t]tau@ is written as application @(t.tau) tau'@, which
 -- in turn is @t_tau tau'@ in the Haskell below.
+module Ch15 where
 
 import Text.Printf
 
@@ -221,18 +256,25 @@ data{-kind-} F = F :+: F
 -- | Expressions of type @[a/X]tau@, where @tau@ is a polynomial
 -- functor.
 data E (tau :: F) a where
-  L :: E f1 a -> E (f1 :+: f2) a           -- ^ Left sum intro.
-  R :: E f2 a -> E (f1 :+: f2) a           -- ^ Right sum intro.
-  P :: E f1 a -> E f2 a -> E (f1 :*: f2) a -- ^ Pair intro.
-  B :: Show b => b -> E (T b) a            -- ^ Base type intro.
-  V :: a -> E X a                          -- ^ Term of variable type intro.
+  -- | Left sum intro.
+  L :: E f1 a -> E (f1 :+: f2) a
+  -- | Right sum intro.
+  R :: E f2 a -> E (f1 :+: f2) a
+  -- | Pair intro.
+  P :: E f1 a -> E f2 a -> E (f1 :*: f2) a
+  -- | Base type intro.
+  B :: Show b => b -> E (T b) a
+  -- | Term of variable type intro.
+  V :: a -> E X a
 deriving instance Functor (E tau)
 deriving instance Show a => Show (E tau a)
 
 ----------------------------------------------------------------
 -- * Eliminators for 'E'
 --
--- Not used.
+-- Using these eliminators allows us to avoid pattern matching on 'E'
+-- expressions in our solutions, which ensures we're working in
+-- Harper's Ch 15 language.
 
 -- | Left product elimination.
 l :: E (f1 :*: f2) a -> E f1 a
@@ -248,20 +290,34 @@ caseOf x_e1 y_e2 e = case e of
   L x -> x_e1 x
   R y -> y_e2 y
 
--- | Term of variable type elimination.
+-- | Variable type elimination.
 v :: E X a -> a
 v (V x) = x
 
+-- | Base type elimination.
+b :: E (T b) a -> b
+b (B x) = x
+
 ----------------------------------------------------------------
--- * Inductive nats via 'F'-functors
+-- * Inductive nats via 'E'-functors
 --
 -- This repeats the examples from the first definition of nats above,
--- but using 'F'-functors.
+-- but using 'E'-functors.
 
 -- | Polynomial functor for nats.
 type Nat'F = T () :+: X
 -- | Nats.
 type Nat' = Ind (E Nat'F)
+
+-- | Eliminator for 'Nat'F'.
+--
+-- Note that 'Nat'F' isomorphic to 'Maybe'!
+--
+-- We can use this to simplify pattern matching on 'Nat'F'; compare
+-- in 'addNat''.
+caseNat'F :: b -> (a -> b) -> E Nat'F a -> b
+caseNat'F caseZ caseS =
+  caseOf (const caseZ) (caseS . v)
 
 -- | Zero.
 z' :: Nat'
@@ -276,8 +332,10 @@ prettyNat' :: Nat' -> String
 prettyNat' = rec @(E Nat'F) x_e1
   where
     x_e1 :: E Nat'F String -> String
-    x_e1 (L (B ())) = "z'"
-    x_e1 (R (V a)) = "s' "++a
+    -- Equivalent 'caseNat'F' version:
+    -- x_e1 = caseNat'F "z'" ("s' "++)
+    x_e1 = caseOf (\_ -> "z'")
+                  (\x -> "s' "++v x)
 
 threeNat' :: Nat'
 threeNat' = s' (s' (s' z'))
@@ -286,9 +344,10 @@ addNat' :: Nat' -> Nat' -> Nat'
 addNat' m n = rec @(E Nat'F) x_e1 m
   where
     x_e1 :: E Nat'F Nat' -> Nat'
-    x_e1 (L (B ())) = n
-    x_e1 (R (V a)) = Fold (R (V a))
-
+    -- Equivalent 'caseNat'F' version:
+    -- x_e1 = caseNat'F n s'
+    x_e1 = caseOf (\_ -> n)
+                  (\x -> s' (v x))
 sixNat' :: Nat'
 sixNat' = addNat' threeNat' threeNat'
 
